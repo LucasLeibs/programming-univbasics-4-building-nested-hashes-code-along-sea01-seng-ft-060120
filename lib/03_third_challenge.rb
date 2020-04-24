9aa0e58cb12e652405b393e7dfd42d1d2c775fcd
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-04-24 18:55:46 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,22 +2,22 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
-        },
+        :name => "Lady Montague"
       :hero => {
-        #your key/value pairs here
+        
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        
         },
       :matriarch => {
-        #your key/value pairs here
+       
         },
       :heroine => {
         #your key/value pairs here
