9f1e7f63d4383956559e49b75fe5cb3116d99c42
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-04-24 19:15:49 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,5 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-    :patriarch => {},
-    :matriarch => {},
-    :hero => {},
-    :hero_friends => []
-      }, 
-    :capulet => {
-     :patriarch => {},
-     :matriarch => {},
-     :herione => {},
-     :herione_friends => []
-      }
-  }
+ 
 
   
 end
